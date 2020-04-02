1894d9045ca59fb427b2c774f1ec123e37753b47
lunatik752 <whitemaxim752@gmail.com>
2020-04-02 14:08:58 +0300

    Updates

---
+++ precache-manifest.8b8ac26af76b493c6239d75f48a941a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f796d49e994a38f3b97a52d927ef6a9b",
+    "url": "/lunatik752/myPortfolio/index.html"
+  },
+  {
+    "revision": "8d6be52a0641c3e6f274",
+    "url": "/lunatik752/myPortfolio/static/css/main.97642397.chunk.css"
+  },
+  {
+    "revision": "af4ce145f0258a22a2e5",
+    "url": "/lunatik752/myPortfolio/static/js/2.478b19db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lunatik752/myPortfolio/static/js/2.478b19db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d6be52a0641c3e6f274",
+    "url": "/lunatik752/myPortfolio/static/js/main.dbe7fde8.chunk.js"
+  },
+  {
+    "revision": "17584ac810c16ddb1414",
+    "url": "/lunatik752/myPortfolio/static/js/runtime-main.b961dc99.js"
+  }
+]);
