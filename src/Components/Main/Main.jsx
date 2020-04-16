290495e8cc7c4860f4acbdf61901037ad257b4c0
lunatik752 <whitemaxim752@gmail.com>
2020-04-16 20:59:38 +0300

    added photo and fonts to main

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -7,11 +7,11 @@ function Main() {
             <div className={styles.container}>
                 <div className={styles.greeting}>
                     <span>Привет!</span>
-                    <span>Меня зовут Максим Белый</span>
-                    <span>Я front-end разработчик</span>
+                    <span>Меня зовут <label>Максим </label><span>Белый</span></span>
+                    <h1>Я front-end разработчик</h1>
                 </div>
                 <div className={styles.photo}>
-                <img src="" alt=""/>
+                <img src='https://lh3.googleusercontent.com/OLJoYlE8bbSKXkjJkVv_sJdm_RkmuKaBNDijcs-bQGfilp1GP8Kc39Lnl4uABcdERYuwvjMPovR3xDRkV8Q4c5ChqGX_-JczIAkTagdcyfZOiXzPaQ2Yyg0hDmGb2sOuSrQNyAl2kKzD40SW9y0gvH3Kx4jOw5qZAEXPJzRPUmme_E_okKPidKXwA6_Sbx19CRQWkFJdIb6bTa8yvsF14HVHJsPa89V63ACRJHxUUt5_dsPGlHc7FgiyhO_JpwlDm5j0xDWgMj-e1sZcXFrnl9_9WJogDcr0WsKzYmQOSv1fmcSSc7f0aQVX8T7OGEQn_LvhzbSE_GkAdoaNpqQrT_fzT1VvugK-pJ7h1Wbe-b2jRk3szDBgUG4IcBFKjPLQEz2b2jKxTl7hpMGrCZSposlGcp8bdaQ3j6fPCwfNhR8yHQOolTpVk550nhki_F5kYgevPkW_UyFPzhrjmziAg7sRVPzlA1tu52I2gUZvSw9un_jVVDhovIO6EdRcuRGYg6qWCkqmyXmcf35iOF9GI6M18royy9j8ZO9Mds2p-JEEQ4GV13j07OplT3YGOKrcc2Liq2Zn4jovrvBdQhgmvVVnQaDQa3pT4cOPEvFpS4nkb5hC9FUaXN_b3QJjbk8FEl5CnWLmhMYalKMwX0Pjp3jA9vvVKzEaRDt8Q01t0RAaBiQJkUpNWk-yIB13=w728-h961-no' alt='myPhoto'/>
                 </div>
             </div>
         </div>
--- src/Components/Slogan/Slogan.jsx
+++ src/Components/Slogan/Slogan.jsx
@@ -6,7 +6,7 @@ function Slogan() {
         <div className={styles.slogan}>
             <div className={styles.container}>
                 <div className={styles.sloganText}>
-                    <span>Рассматриваю варианты удаленной работы</span>
+                    <span>LET'S WORK TOGETHER!</span>
                 </div>
                 <div className={styles.sloganButton}>
                     <button>Нанять меня</button>
