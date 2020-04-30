bc14edfff69a6fa41fdafeadb4f3f7bdb4493051
lunatik752 <whitemaxim752@gmail.com>
2020-04-30 19:07:09 +0300

    fixed footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,34 +2,34 @@ import React from 'react';
 import styles from './Footer.module.css';
 
 function Footer() {
-  return (
-    <div className={styles.footer}>
-      <div className={styles.container}>
-        <div className={styles.footerTitle}>
-          <span>Максим Белый</span>
-        </div>
-        <div className={styles.myLinks}>
-          <div className={styles.link}>
-            <span>f</span>
-          </div>
-          <div className={styles.link}>
-            <span>vc</span>
-          </div>
-          <div className={styles.link}>
-            <span>in</span>
-          </div>
-          <div className={styles.link}>
-            <span>GitHub</span>
-          </div>
+    return (
+        <div className={styles.footer}>
+            <div className={styles.container}>
+                <div className={styles.footerTitle}>
+                    <span>Максим Белый</span>
+                </div>
+                <div className={styles.myLinks}>
+                    <div className={styles.link}>
+                        <span>f</span>
+                    </div>
+                    <div className={styles.link}>
+                        <span>vc</span>
+                    </div>
+                    <div className={styles.link}>
+                        <span>in</span>
+                    </div>
+                    <div className={styles.link}>
+                        <span>GitHub</span>
+                    </div>
+
+                </div>
+                <div className={styles.copyright}>
+                    <span>© 2020 Все права защищены </span>
+                </div>
+            </div>
 
         </div>
-        <div className={styles.copyright}>
-          <span>© 2020 Все права защищены </span>
-        </div>
-      </div>
-
-    </div>
-  );
+    );
 }
 
 export default Footer;
