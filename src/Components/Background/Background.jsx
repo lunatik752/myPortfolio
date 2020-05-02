a7971895867b57d8de238dfa1772477d864d42c3
lunatik752 <whitemaxim752@gmail.com>
2020-05-02 21:35:08 +0300

    fixed background

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './Background.module.css';
-import img from '../../assets/image/bg1.jpg'
+// import img from '../../assets/image/bg1.jpg'
 
 
 const Background = () => {
     return (
         <div className={styles.backgroundWrapper}>
-            <img className={styles.background} src={img} alt=""/>
+            {/*<img className={styles.background} src={img} alt=""/>*/}
         </div>
     );
 }
