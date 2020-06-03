675bb712ef13209ccc709cc474ae23224089e00e
lunatik752 <whitemaxim752@gmail.com>
2020-06-03 22:03:19 +0300

    added Particles

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React from 'react';
-import './App.css';
-import Header from "./Components/Header/Header";
+import styles from './App.module.css';
+import Header from './Components/Header/Header';
 import Main from "./Components/Main/Main";
 import Skills from "./Components/Skills/Skills";
 import Slogan from "./Components/Slogan/Slogan";
 import Projects from "./Components/Projects/Projects";
 import Contacts from "./Components/Contacts/Contacts";
 import Footer from "./Components/Footer/Footer";
+import Particles from 'react-particles-js';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="portfolioWrapper">
-         <Header/>
-         <Main/>
-         <Skills/>
-         <Projects/>
-         <Slogan/>
-         <Contacts/>
-         <Footer/>
-      </div>
 
-    </div>
-  );
+const particlesOptions = {
+    particles: {
+        number: {
+            value: 100,
+            density: {
+                enable: true,
+                value_area: 1000
+            }
+        },
+        color: {
+            value: '#ff4500'
+        },
+        shape: {
+            type: 'circle',
+            stroke: {
+                width: 3,
+                color: '#ff4500'
+            }
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: '#ffffff',
+            opacity: 0.3,
+            width: 1
+        }
+    }
+}
+
+const App = () => {
+    return (
+        <div className={styles.app}>
+                <Particles className={styles.particles} params={particlesOptions}/>
+                <Header/>
+                <Main/>
+                <Skills/>
+                <Projects/>
+                <Slogan/>
+                <Contacts/>
+                <Footer/>
+        </div>
+    );
 }
 
 export default App;
